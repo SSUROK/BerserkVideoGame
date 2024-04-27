3197d08d7f106405c066cf090a18a64793557e3e
Ilay Vinogradov <ilay125114@gmail.com>
2024-04-27 03:43:48 +0300

    added first level, walk anim, idle anim, movement, fake chest use v2

---
+++ assets/data/maps/map1/chests.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="chests" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="chests-small-alpha.png" width="256" height="160"/>
+</tileset>
